7597725faac9fea30068ccf72bf7602967af4d4e
Logan Dahm <ldgcc@umsystem.edu>
2026-06-06 08:11:50 -0700

    Cleaned up directory structure

--- c++/handyfuncs.cpp
+++ c++/handyfuncs.cpp
@@ -17,10 +17,15 @@ array<int, 2> vectorize(int rot) {
         return {0,-1};
         break;
     default:
+        return {0,0};
         break;
     }
 }
 
 array<int, 2> addVec2(array<int, 2> lhs, array<int, 2> rhs) {
     return {lhs[0]+rhs[0], lhs[1]+rhs[1]};
+}
+
+array<int, 2> negVec2(array<int, 2> vec) {
+    return {-vec[0],-vec[1]};
 }
--- c++/test.cpp
+++
@@ -1,7 +0,0 @@
-#include "scene.h"
-using namespace std;
-
-int main() {
-    Scene s;
-    s.display();
-}
